e4dca01a3a3739fa0174eb27351f9dc5d8966f8a
Anucha Ananbenjapon <anucha_a@hotmail.com>
2021-11-12 14:51:37 +0700

    Add files via upload

--- ch06/Ex6_13.py
+++ ch06/Ex6_13.py
@@ -1,5 +1,6 @@
 fruit = ["apple","orange","kiwi","grape","banana"]
 print(fruit)
 fruit.insert(0,"cherry")
+print("หลัง insert cherry =>",fruit)
 fruit.insert(2,"papaya")
-print(fruit)
+print("หลัง insert papaya =>",fruit)
